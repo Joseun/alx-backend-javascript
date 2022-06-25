72ab9435ecd6964711a93885cde93deb3c8f977f
Joseun <ologanj@gmail.com>
2022-06-25 19:34:47 +0000

    ES6 Basics

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,4 +1,4 @@
-// function appendToEachArrayValue to use ES6’s for...of operator. 
+// function appendToEachArrayValue to use ES6’s for...of operator.
 // And don’t forget that var is not ES6-friendly.
 export default function appendToEachArrayValue(array, appendString) {
   const array2 = [];
--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,4 +1,4 @@
 // function to return the number of arguments passed to it using the rest parameter syntax
 export default function returnHowManyArguments(...args) {
-    return args.length;
+  return args.length;
 }
