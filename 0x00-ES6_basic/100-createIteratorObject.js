8251179e34bcd99948311cbc9003eef86b7ebe28
Joseun <ologanj@gmail.com>
2022-06-25 19:11:09 +0000

    ES6 Basics

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -3,9 +3,9 @@
 // This function will return an iterator to go through
 //every employee in every department
 export default function createIteratorObject(report) {
-    let result = [];
-    for (const value of Object.values(report.allEmployees)) {
-        result = [...result, ...value];
+  let result = [];
+  for (const value of Object.values(report.allEmployees)) {
+    result = [...result, ...value];
   }
   return result;
 };
